b0585ac1e39f59de3c0b2511400f954eecfbbcae
Ahmad Ahandani <a.e.ahandani@gmail.com>
2024-01-04 22:35:56 -0800

    update folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-import { MyElement as ml } from "./components/ml/my-element"
-import { MyElement as mx } from "./components/md/my-element"
-export const MyElement = ml
-export const Element = mx
-
+import { MyElement } from "./components/ml/my-element"
+export default { MyElement }
