0d9ea42e383903fca8fb3e0cac5f4441a7078004
Ahmad Ahandani <a.e.ahandani@gmail.com>
2024-01-07 20:40:42 -0800

    feat: add tailwind css

--- src/components/menu/index.ts
+++ src/components/menu/index.ts
@@ -1,22 +1,26 @@
 import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
+import { TWStyles } from "../../tailwind";
 
-const componentName = "wd-head";
+const componentName = "wd-menu";
 
 @customElement(componentName)
-export class Header extends LitElement {
-  static override styles = css`
+export class Menu extends LitElement {
+  static override styles = [css`
     :host{
       display: flex;
       border: solid 1px red;
       width: 100%;
       margin: 0;
     }
-  `;
+  `, TWStyles];
 
   override render() {
     return html`
-      <h1 class=" bg-red-500">Header</h1>
+          <h1 class="text-4xl font-bold underline bg-slate-100">
+    Hello world!
+  </h1>
+      <button class="bg-red">Menu</button>
       <slot></slot>
     `;
   }
@@ -25,6 +29,6 @@ export class Header extends LitElement {
 
 declare global {
   interface HTMLElementTagNameMap {
-    [componentName]: Header;
+    [componentName]: Menu;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Header } from "./components/header"
+import { Menu } from "./components/menu"
 import { Footer } from "./components/footer"
 import { Content } from "./components/content"
-export default { Header, Footer, Content }
+export default { Header, Footer, Content, Menu }
